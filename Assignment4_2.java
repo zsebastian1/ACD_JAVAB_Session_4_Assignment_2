a6000f2a1cee1e49c3254b4162e1da888bda8e8c
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-12 09:42:53 -0400

    Add files via upload

---
+++ Assignment4_2.java
@@ -0,0 +1,17 @@
+package ACADAssignments;
+
+public class Assignment4_2 {
+
+	public static void main(String[] args) {
+
+		//implicit type promotion
+		
+		int x = 11;
+		int y = 44;
+		
+		double answer = y / x;
+		
+		System.out.println(answer);
+	}
+
+}
